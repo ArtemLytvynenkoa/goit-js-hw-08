9b387082a48f8f3baf7adf7d13c50b351e5a2b74
ArtemLytvynenkoa <chpart355@gmail.com>
2021-11-23 19:36:58 +0200

    adds edits to third task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,7 +2,9 @@ import Throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
 const feedbackFormData = {};
-   
+const saveFeedbackFormData = localStorage.getItem('feedback-form-state');
+const parsedFeedbackFormData = JSON.parse(saveFeedbackFormData);
+
 
 form.addEventListener('submit', onFormSubmit);
 form.addEventListener('input', Throttle(onFormInput, 500));
@@ -17,25 +19,22 @@ function onFormInput(evt) {
 
 function onFormSubmit(evt) {
     evt.preventDefault();
+    console.log(parsedFeedbackFormData);
     evt.currentTarget.reset();
     localStorage.removeItem('feedback-form-state');
 };
 
 function repairFormsData() {
-    const saveFeedbackFormData = localStorage.getItem('feedback-form-state');
-    const parsedFeedbackFormData = JSON.parse(saveFeedbackFormData);
-    
-    console.log(parsedFeedbackFormData);
-    
     if (parsedFeedbackFormData) {
         form.email.value = parsedFeedbackFormData.email;
         form.message.value = parsedFeedbackFormData.message;
-        
+
         if (!parsedFeedbackFormData.email) {
-        form.email.value = '';
+            form.email.value = '';
         };
         if (!parsedFeedbackFormData.message) {
             form.message.value = '';
         }
     };
+
 }
